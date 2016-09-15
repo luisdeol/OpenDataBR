e0d0346181284f0e59de98d5acb0218a0f28a9a3
luisfelipekai <luisfelipekai@gmail.com>
2016-09-15 11:48:51 -0300

    #FUNTTEL getComercializaveis
    getComercializaveis funcionando.

--- FUNTTEL/urls.py
+++ FUNTTEL/urls.py
@@ -2,6 +2,8 @@ from django.conf.urls import patterns, url
 
 urlpatterns = patterns(
     'FUNTTEL.views',
-        url(r'^getTaxaRetorno/$', 'getTaxaRetorno', name='getTaxaRetorno'),
-        url(r'^getGeracaoEmprego/$', 'getGeracaoEmprego', name='getGeracaoEmprego'),
+    url(r'^getTaxaRetorno/$', 'getTaxaRetorno', name='getTaxaRetorno'),
+    url(r'^getGeracaoEmprego/$', 'getGeracaoEmprego', name='getGeracaoEmprego'),
+    url(r'^getComercializaveis/$', 'getComercializaveis', name='getComercializaveis'),
+    url(r'^getPropriedadeIntelectualBRExterior/$', 'getPropriedadeIntelectualBRExterior', name='getPropriedadeIntelectualBRExterior')
 )
--- FUNTTEL/views.py
+++ FUNTTEL/views.py
@@ -5,7 +5,7 @@ from pymongo import MongoClient
 from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from FUNTTEL.FUNTTEL_Utils import updateDB
+from FUNTTEL.FUNTTEL_Utils import updateDB, connectDb
 from bson.json_util import dumps
 import json
 
@@ -29,3 +29,22 @@ def getGeracaoEmprego(request):
     jsonstrindent = json.loads(jsonstr)
     #updateDB()
     return Response(jsonstrindent)
+
+@api_view(['GET'])
+def getComercializaveis(request):
+    db = connectDb()
+    comercializaveis = db.InovaçãoTecnológicaProdutoseTecnologiasComercializáveis
+    jsonstr = dumps(comercializaveis.find())
+    jsonstrindent = json.loads(jsonstr)
+    #updateDB()
+    return Response(jsonstrindent)
+
+@api_view(['GET'])
+def getPropriedadeIntelectualBRExterior(request):
+    db = connectDb()
+    propIntelectual = db.InovaçãoTecnológicaPropriedadeIntelectualBrasileExterior
+    jsonstr = dumps(propIntelectual.find())
+    jsonstrindent = json.load(jsonstr)
+    #updateDB()
+    return Response(jsonstrindent)
+
